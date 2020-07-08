c40d379b4d3679ba7edd220db2a530b6649a4735
luzheminlulu <luzheminlulu@hotmail.com>
2020-07-08 16:50:01 +0800

    Update set_picture.py

--- set_picture.py
+++ set_picture.py
@@ -8,11 +8,13 @@ import NICT_Download
 import weather
 import os
 
+dir  =  "D:/himawari8_background-master/"
 
 name = ["滁州"     ,     "杭州" , "" ,     "上海",   ] #城市名(建议少于等于6个)
 city = ["chuzhou"  , "hangzhou" , "" , "shanghai",   ] #城市拼音
 Lng  = [       118 ,        120 , 0  ,        121,   ] #经度  (建议60~140~180)
 Lat  = [        32 ,         30 , 0  ,         31,   ] #纬度  (建议-80~80)
+
 
 def clear_dir(path):
 	print("正在删除%s下的文件"%(path))
@@ -33,10 +35,10 @@ def set_desktop_windows(imagepath):
 
 if __name__ == '__main__':
 	try:
-		#clear_dir("D:/himawari8_background-master/__pycache__/")
-		clear_dir("D:/himawari8_background-master/Download_Picture/")
-		clear_dir("D:/himawari8_background-master/Wallpaper/")
-		img_save_path = NICT_Download.dl_main()
+		#clear_dir(dir+"__pycache__/")
+		clear_dir(dir+"Download_Picture/")
+		clear_dir(dir+"Wallpaper/")
+		img_save_path = NICT_Download.dl_main(dir)
 		weather.draw_weather(city,name,Lng,Lat,img_save_path)
 		set_desktop_windows(img_save_path)
 	except Exception as e:
